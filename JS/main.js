22b369ebfb5c958bed84a2b5ba3e97a6123cfe6c
JefersonA <araujo_jef@hotmail.com>
2020-05-30 20:11:41 -0300

    Enviado Pasta JavaScript

---
+++ JS/main.js
@@ -0,0 +1,45 @@
+
+/*
+alert("Vamos começar");
+var n1=parseInt(prompt ("Digite um numero"));
+var n2=parseInt(prompt ("Digite outro numero"));
+
+console.log(n1);
+console.log(n2);
+alert(n1+n2);
+*/
+
+
+function soma(n1,n2){
+var n1=parseInt(n1);
+var n2=parseInt(n2);
+    document.getElementById("resultado").innerHTML=n1+n2;
+
+}
+function subtrai(n1,n2){
+    var n1=parseInt(n1);
+    var n2=parseInt(n2);
+    document.getElementById("resultado").innerHTML=n1-n2;
+}
+
+function divide(n1,n2){
+    
+    var n1=parseInt(n1);
+    var n2=parseInt(n2);
+    document.getElementById("resultado").innerHTML=n1/n2;
+}
+
+function multiplica(n1,n2){
+    
+    var n1=parseInt(n1);
+    var n2=parseInt(n2);
+    document.getElementById("resultado").innerHTML=n1*n2;
+}
+function inserir(numero){
+document.getElementById("valor").innerHTML=num1.value;
+console.log();
+
+}
+
+
+
